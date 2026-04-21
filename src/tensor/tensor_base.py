2af066e094720ec42c060e008d89269991e2f66f
lazarzivanovicc <lazar.zivanovic11@gmail.com>
2026-04-21 23:24:24 +0200

    baby tensor and autograd

---
+++ src/tensor/tensor_base.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+class Tensor:
+    def __init__(self, data: np.ndarray | list, prev: set = None):
+        self.data: np.ndarray = data if isinstance(data, np.ndarray) else np.array(data)
+        self.grad: np.ndarray = np.zeros_like(data)
+        self._backward = lambda: None
+        self._prev: set = set() if prev is None else prev
+        # requires_grad boolean - if it is not true pytorch wont allow backward call
+        # additionally pytorch does not allow grads for ints, only floats and complex
+        # device
+        # dtype
+
+    
+    def __add__(self, other) -> Tensor:
+        out: Tensor = Tensor(self.data + other.data, {self, other})
+        def backward() -> None:
+            self.grad += out.grad * 1
+            other.grad += out.grad * 1
+        out._backward = backward
+        return out
+
+
+    def __matmul__(self, other) -> Tensor:
+        # assert self.data.shape[-1] == other.data.shape[-2] / "Error dimensions not match"
+        out: Tensor = Tensor(self.data @ other.data, {self, other})
+        def backward() -> None:
+            # X @ W = Z
+            self.grad += out.grad @ other.data.T
+            other.grad += self.data.T @ out.grad
+        out._backward = backward
+        return out
+
+    
+    def backward(self) -> None:
+        # This is my initialization step
+        self.grad: np.ndarray = np.ones_like(self.data)
+        visited: set = set()
+        topo: list[Tensor] = []
+        def build_topo(t: Tensor) -> None:
+            if t not in visited:
+                visited.add(t)
+                for p in t._prev:
+                    build_topo(p)
+                topo.append(t)
+        build_topo(self)
+        for t in reversed(topo):
+            t._backward()
+
+
+# Separate Function as an independent class, this clojure approach is not good and this will not scale well
+# Each new function should extends Function and define it's forward and backward and it should save it's parents along with the context needed for backward pass
+# Tensor will hold only API for interacting with these fn's
+# Once we change that we will have FN-centric graph and not Tensor-centric like now
+
---
+++ src/tests/test.py
@@ -0,0 +1,15 @@
+import numpy as np
+import torch
+ 
+from src.tensor.tensor_base import Tensor
+
+def run_tests() -> bool:
+    print("ALL TESTS PASSED")
+    return True
+
+
+if __name__ == "__main__":
+    run_tests()
+
+
+
